4e688efdd17195ac4d26215803b94e5204e94471
Jakob Foerste <github@jfoe.de>
2022-07-30 15:18:20 +0200

    add request allocation

--- parser.c
+++ parser.c
@@ -1,5 +1,7 @@
+#include <malloc.h>
 #include "parser.h"
 
 struct request* parse_request(char* str, size_t len) {
-    return 0;
+    struct request* request = malloc(sizeof (struct request));
+    return request;
 }
